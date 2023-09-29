f0e95d3a9806a504b307cd24e299ebf43919ef7b
Dmitry Yavorsky <Yavorskyd@yandex.ru>
2023-09-29 12:32:53 +0300

    Initial Commit

---
+++ My Super Cards/My_Super_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Super_CardsApp.swift
+//  My Super Cards
+//
+//  Created by Yavorsky on 29.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct My_Super_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
